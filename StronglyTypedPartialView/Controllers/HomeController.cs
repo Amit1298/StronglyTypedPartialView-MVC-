d2ff2ab5fd9ec593ecde08ae6d6228b6323a7e06
Amit <amitkr24031995@gmail.com>
2022-03-01 22:39:19 +0530

    Adding StronglyTypedPartialView

---
+++ StronglyTypedPartialView/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using StronglyTypedPartialView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypedPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        List<Product> ProductsList = new List<Product>()
+        {
+            new Product{id=1,name="Reebok Shoes",price=1000.00,picture="~/Images/ReebokShoes.jpg"},
+            new Product{id=2,name="Rayben SunGlasses",price=2000.00,picture="~/Images/Rayben.jpg"},
+            new Product{id=3,name="Rolex Watch",price=4000.00,picture="~/Images/rolex.jpg"}
+        };
+        public ActionResult Index()
+        {
+            return View(ProductsList);
+        }
+    }
+}
